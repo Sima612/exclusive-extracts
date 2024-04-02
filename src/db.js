7adbf1cbd38566f3b6bc005e8553ef930f1e55a5
Sima Tran <64188273+Sima612@users.noreply.github.com>
2024-04-02 00:33:42 -0700

    updated products

--- src/db.js
+++ src/db.js
@@ -1,5 +1,59 @@
 export const products = [
   // More products...
+  {
+    name: "Berry Haze",
+    desc: "Nectarine X Block Berry",
+    href: "#",
+    // imageSrc: "./zourd.png",
+    videoSrc: "./berryHaze.mp4",
+    temp: "Ideal Temperature 520",
+    note: "*** All dab temperatures are based on a serving size of 0.2. Larger or smaller dabs will effect the correct temperature you should be dabbing ***",
+  },
+  {
+    name: "Tallymon",
+    desc: "Tallymon",
+    href: "#",
+    // imageSrc: "./zourd.png",
+    videoSrc: "./tallymon.mp4",
+    temp: "Ideal Temperature 520",
+    note: "*** All dab temperatures are based on a serving size of 0.2. Larger or smaller dabs will effect the correct temperature you should be dabbing ***",
+  },
+  {
+    name: "Sour Yaya",
+    desc: "Papaya X Sour D",
+    href: "#",
+    // imageSrc: "./zourd.png",
+    videoSrc: "./sourYaya.mp4",
+    temp: "Ideal Temperature 510",
+    note: "*** All dab temperatures are based on a serving size of 0.2. Larger or smaller dabs will effect the correct temperature you should be dabbing ***",
+  },
+  {
+    name: "Donald Duck",
+    desc: "Papaya",
+    href: "#",
+    // imageSrc: "./zourd.png",
+    videoSrc: "./donaldDuck.mp4",
+    temp: "Ideal Temperature 520",
+    note: "*** All dab temperatures are based on a serving size of 0.2. Larger or smaller dabs will effect the correct temperature you should be dabbing ***",
+  },
+  {
+    name: "Turbo Grapes",
+    desc: "Grapes N Cream (Gassy Pheno)",
+    href: "#",
+    // imageSrc: "./zourd.png",
+    videoSrc: "./turboGrapes.mp4",
+    temp: "Ideal Temperature 510",
+    note: "*** All dab temperatures are based on a serving size of 0.2. Larger or smaller dabs will effect the correct temperature you should be dabbing ***",
+  },
+  {
+    name: "Super Mario",
+    desc: "Tahoe Cure",
+    href: "#",
+    // imageSrc: "./zourd.png",
+    videoSrc: "./superMario.mp4",
+    temp: "Ideal Temperature 520",
+    note: "*** All dab temperatures are based on a serving size of 0.2. Larger or smaller dabs will effect the correct temperature you should be dabbing ***",
+  },
 ];
 
 export const newProducts = [
