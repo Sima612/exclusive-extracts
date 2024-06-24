62b57abc0c1b0a7a2d1646269255614b290d69a0
Sima Tran <64188273+Sima612@users.noreply.github.com>
2024-06-24 01:25:43 -0700

    new products

--- src/db.js
+++ src/db.js
@@ -58,4 +58,58 @@ export const products = [
 
 export const newProducts = [
   // More products...
+  {
+    name: "Breadstix",
+    desc: "Garlic Breath X Jiffy Cake",
+    href: "#",
+    imageSrc: "./breadstix.jpeg",
+    videoSrc: "./breadstix.mp4",
+    temp: "Ideal Temperature 510",
+    note: "*** All dab temperatures are based on a serving size of 0.15. Larger or smaller dabs will effect the correct temperature you should be dabbing ***",
+  },
+  {
+    name: "Jim Carrey",
+    desc: "Tahoe Cure X Grape Pie",
+    href: "#",
+    imageSrc: "./jimcarrey.jpeg",
+    videoSrc: "./jimcarrey.mp4",
+    temp: "Ideal Temperature 510",
+    note: "*** All dab temperatures are based on a serving size of 0.15. Larger or smaller dabs will effect the correct temperature you should be dabbing ***",
+  },
+  {
+    name: "Suga Suga",
+    desc: "Pink Zugar",
+    href: "#",
+    imageSrc: "./suga.jpeg",
+    videoSrc: "./suga.mp4",
+    temp: "Ideal Temperature 510",
+    note: "*** All dab temperatures are based on a serving size of 0.15. Larger or smaller dabs will effect the correct temperature you should be dabbing ***",
+  },
+  {
+    name: "Dummies",
+    desc: "Tallymon X Dosido",
+    href: "#",
+    imageSrc: "./dummies.jpeg",
+    videoSrc: "./dummies.mp4",
+    temp: "Ideal Temperature 510",
+    note: "*** All dab temperatures are based on a serving size of 0.15. Larger or smaller dabs will effect the correct temperature you should be dabbing ***",
+  },
+  {
+    name: "Sundae Pie",
+    desc: "Sundae Driver X Grape Pie",
+    href: "#",
+    imageSrc: "./sundae.jpeg",
+    videoSrc: "./sundae.mp4",
+    temp: "Ideal Temperature 510",
+    note: "*** All dab temperatures are based on a serving size of 0.15. Larger or smaller dabs will effect the correct temperature you should be dabbing ***",
+  },
+  {
+    name: "Jenny From The Block",
+    desc: "Block Berry X Motor Breath",
+    href: "#",
+    imageSrc: "./jenny.jpeg",
+    videoSrc: "./jenny.mp4",
+    temp: "Ideal Temperature 510",
+    note: "*** All dab temperatures are based on a serving size of 0.15. Larger or smaller dabs will effect the correct temperature you should be dabbing ***",
+  },
 ];
