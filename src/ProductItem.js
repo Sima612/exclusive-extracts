2bab7b5e56463d127d9899e270e6b90a3a22566d
Sima Tran <64188273+Sima612@users.noreply.github.com>
2024-12-10 20:13:02 -0800

    added product

--- src/ProductItem.js
+++ src/ProductItem.js
@@ -1,22 +1,24 @@
 export default function ProductItem({ product }) {
   return (
-    <div className="group grid content-between">
-      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+    <div className="group grid content-around">
+      <div className="aspect-h-1 aspect-w-1 w-64 h-64 overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
         <video
           src={product.videoSrc}
           controls
           loop
-          className="h-full w-full object-cover object-center group-hover:opacity-75"
+          className="h-full w-full object-cover object-center"
         />
       </div>
-      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+
+      <div className="aspect-h-1 aspect-w-1 w-64 overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
         <img
-          className="h-full w-full object-cover object-center group-hover:opacity-75"
+          className="h-full w-full object-cover object-center"
           src={product.imageSrc}
           alt="product images"
         />
       </div>
-      <div className="text-start">
+
+      <div className="text-start w-64">
         <h4 className="mt-2 text-xl font-medium text-gray-700">
           {product.name}
         </h4>
